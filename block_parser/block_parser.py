518317f95c153205339522c8784f8767844b20c5
Nikola Dokoski <nikola.dokoski@vapour-apps.com>
2018-03-15 16:19:01 +0100

    Block parser now works with containing blocks. Probably.

--- block_parser/block_parser.py
+++ block_parser/block_parser.py
@@ -7,7 +7,31 @@ test_block = {
     "block_data" : {
         "bpm" : 120,
         "instrument" : "piano",
-        "blocks" : []
+        "blocks" : [{
+                "block_data" : {
+                    "bpm" : 140
+                }, 
+                "structure_data" : {
+                    "notes_data" : {
+                        "chord_root" : "C",
+                    }, 
+                    "timing_data" : {
+                        "starting_beat" : 0
+                    }
+                }
+            }, {
+                "block_data" : {
+                }, 
+                "structure_data" : {
+                    "notes_data" : {
+                        "chord_root" : "F",
+                    }, 
+                    "timing_data" : {
+                        "starting_beat" : 14
+                    }
+                }
+            }, 
+        ]
     },
     "structure_data" : {
         "notes_data" : {
@@ -27,30 +51,32 @@ test_block = {
 
 #TODO finish writing up what should be stored in this data. 
 
-#Blocks can hold other blocks, which inherit any data from parent blocks which they don't define. For instance: 
 
-{
-    "block_data" : {
-        "id" : "main",
-        "bpm" : 120, 
-        "instrument" : "piano",
-        "blocks" : [        
-            {
-                "block_data" : {
-                    "instrument" : "trumpet", 
-                    "id" : "chorus",
-                },
-                "structure_data" : {
-                    'data' : '...'
-                }
-            }
-        ]
-    }
-}
+#Updates a child block with its parent's data if the child doesn't have that key populated. Allows certain keys to be excluded. 
+def update_child_from_parent(child, parent, excluded_keys = []):
+    for key in parent: 
+        if key not in child and key not in excluded_keys: 
+            child[key] = parent[key]
 
+    return child
+   
 
-def parse_block(block):
+def get_block_notes(block, parent_data = {}):
 
+    #If this is a child block, we first update any data only present in its parents. 
+    if parent_data: 
+        block['block_data'] = update_child_from_parent(block['block_data'], parent_data['block_data'], excluded_keys = ['blocks', 'id'])
+        block['structure_data']['notes_data'] = update_child_from_parent(block['structure_data']['notes_data'], parent_data['structure_data']['notes_data'])
+        block['structure_data']['timing_data'] = update_child_from_parent(block['structure_data']['timing_data'], parent_data['structure_data']['timing_data'])
+
+    #If this is a block containing other blocks, it doesn't generate notes by itself. 
+    #Instead it just gets notes from its child blocks. 
+    if block['block_data'].get('blocks'): 
+        notes = []
+        for child_block in block['block_data']['blocks']: 
+            notes += get_block_notes(child_block, parent_data = block)
+        return notes
+   
     #Returns a list of dicts, depicting bars with data about where the bar starts, and data about each note, like so: [{"start" : 0, "notes" : []}, {"start" : 3, "notes" : []}]
     #Each note has a start, length and volume; start and length are based on the bar pattern, volume is based on the accents
     bars = timing_organizer.get_timings(**block['structure_data']['timing_data'])
@@ -62,7 +88,13 @@ def parse_block(block):
     #Then we update the notes with the instruments, which may modify the volume (for instance, trumpet may be loud, so it should have a multiplier, like {"insturment" : "trumpet", "volume" : 0.5, "program_number" : 123}, not the actual program_number tho.
     notes = instrument_handler.add_instrument_data_to_notes(notes = notes, instrument_name = block['block_data']['instrument'])
 
-    #We finally have a list of notes in dictionary form with all the data needed to convert them to midi. 
+    return notes
+
+
+def parse_block(block):
+    notes = get_block_notes(block)
+
+    #We have a list of notes in dictionary form with all the data needed to convert them to midi. 
     midi_file = midi_handler.write_notes_to_midi(notes)
 
     #midi_file should just be a filename. We want to make it into a wav. 
--- block_parser/timing_organizer.py
+++ block_parser/timing_organizer.py
@@ -4,8 +4,17 @@ import datetime
 #Gets a dictionary containing data on how to order notes. 
 def get_timings(starting_beat = 0, bar_length = 4, number_of_bars = 4, pattern = [], accents = {}, base_volume = 50):
     bars = [{"start" : 0, "base_volume" : base_volume, "notes" : [{"start" : 0, "length" : 1}, {"start" : 1, "length" : 2}, {"start" : 2, "length" : 1}]}, {"start" : 3, "base_volume" : base_volume, "notes" : [{"start" : 0, "length" : 1}, {"start" : 1, "length" : 2}, {"start" : 2, "length" : 1}]}]
+    bars = handle_timing(bars)
     bars = handle_accenting(bars, accents)
     return bars
+
+def handle_timing(bars):
+    for bar in bars: 
+        for note in bar['notes']: 
+            note['start'] += bar['start']
+ 
+    return bars
+   
 
 #Gets the timed list of notes and increases the volume of the notes based on the accent_list
 #Example, in a list with 6 notes, the accent_list {0 : 10, 2 : 20} will increase the 
